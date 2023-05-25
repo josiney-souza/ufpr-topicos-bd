e585c0488542c1879aaa4e6e33e3633f5b154cd6
Josiney de Souza <josiney.souza@ifc.edu.br>
2023-05-25 11:23:53 -0300

    Criado os arquivos p3-somar-burocratas.sh e p3-somar-diretoras.sh
    Foram criados os arquivos p3-somar-burocratas.sh e p3-somar-diretoras.sh para realizar os passos 3 e 4 do workflow (somar/contar a quantidade de votos de alguma questão e criar a estatísticas sobre as votações, respectivamente). Atualmente, os scripts estão calculando os valores para as questões tratadas no quadro 5 da tese de doutorado da autora (Respostas das burocratas entrevistadas sobre a autorização da direção da PFP para a realização de visitas íntimas - https://hdl.handle.net/1884/70370)
    São dois arquivos porque, para a base dos diretores, as respostas estão na ordem "Não", Sim", "Não sabe"; na outra base, de demais burocratas, as respostas possíveis 1 e 3 estão invertidas, ou seja, "Não sabe", "Sim", "Não". Além de deixar a execução mais didática

---
+++ trabalho2/p3-somar-diretoras.sh
@@ -0,0 +1,57 @@
+#! /bin/bash
+
+if [ ! $1 ]; then echo "ERRO: argumento 1 (\$1 - BD) vazio"; exit 1
+elif [ ! -e $1 ]; then echo "ERRO: argumento 1 (\$1 - BD) inexistente"; exit 1
+elif [ ! $2 ]; then echo "ERRO: argumento 2 (\$2 - CHAR) vazio"; exit 2
+elif [ ! $3 ]; then echo "ERRO: argumento 3 (\$3 - COLUNA) vazio"; exit 3
+elif [ ! $4 ]; then echo "ERRO: argumento 4 (\$4 - INDIC) vazio"; exit 4; fi
+
+NQUESTAO=$(cut -d$2 -f$3 $1 | head -n1)
+QUESTAO=$(cut -d$2 -f$3 $1 | tail -n+2)
+
+NAO=0
+SIM=0
+NSABE=0
+NRESP=0
+for VALOR in $QUESTAO
+do
+	if [[ $VALOR == 1 ]]
+	then
+		NAO=$(( $NAO + 1 ))
+	elif [[ $VALOR == 2 ]]
+	then
+		SIM=$(( $SIM + 1 ))
+	elif [[ $VALOR == 3 ]]
+	then
+		NSABE=$(( $NSABE + 1 ))
+	elif [[ $VALOR == 0 ]]
+	then
+		NRESP=$(( $NRESP + 1 ))
+	elif echo $VALOR | grep -q ";"
+	then
+		echo "Votaram em mais de uma opcao: $VALOR"
+		if echo $VALOR | grep -q "1"
+		then
+			NAO=$(( $NAO + 1 ))
+		fi
+		if echo $VALOR | grep -q "2"
+		then
+			SIM=$(( $SIM + 1 ))
+		fi
+		if echo $VALOR | grep -q "3"
+		then
+			NSABE=$(( $NSABE + 1 ))
+		fi
+	else
+		echo "Valor \'$VALOR\' invalido"
+	fi
+done
+
+echo "=== Resultados para $NQUESTAO ($4) ==="
+echo "Quantidade de NAO: $NAO"
+echo "Quantidade de SIM: $SIM"
+echo "Quantidade de NSABE: $NSABE"
+echo "Quantidade de NRESP: $NRESP"
+echo
+
+exit 0
