3eeab59828ca25c3c21f0090d3576c2a5c718055
Josiney de Souza <josiney.souza@ifc.edu.br>
2023-08-21 15:11:52 -0300

    sed(1) em p5-cria-tabela.sh otimizado
    O comando sed(1) existente no script p5-cria-tabela.sh foi otimizado. Anteriormente, haviam dois sed(1); agora existe um apenas que foram juntados.

--- trabalho2/p5-cria-tabela.sh
+++ trabalho2/p5-cria-tabela.sh
@@ -13,8 +13,8 @@ PNAO=$(echo "scale=${ESCALA}; ${NAO}*100 / 7" | bc -l)
 PNSABE=$(echo "scale=${ESCALA}; ${NSABE}*100 / 7" | bc -l)
 sed -i -e "s/PDIR-EXT-SIM/${PSIM}/g" \
 	-e "s/PDIR-EXT-NAO/${PNAO}/g" \
-	-e "s/PDIR-EXT-NSABE/${PNSABE}/g" $TABELA
-sed -i -e "s/DIR-EXT-SIM/${SIM}/g" \
+	-e "s/PDIR-EXT-NSABE/${PNSABE}/g" \
+	-e "s/DIR-EXT-SIM/${SIM}/g" \
 	-e "s/DIR-EXT-NAO/${NAO}/g" \
 	-e "s/DIR-EXT-NSABE/${NSABE}/g" $TABELA
 
@@ -26,8 +26,8 @@ PNAO=$(echo "scale=${ESCALA}; ${NAO}*100 / 7" | bc -l)
 PNSABE=$(echo "scale=${ESCALA}; ${NSABE}*100 / 7" | bc -l)
 sed -i -e "s/PDIR-MASC-SIM/${PSIM}/g" \
 	-e "s/PDIR-MASC-NAO/${PNAO}/g" \
-	-e "s/PDIR-MASC-NSABE/${PNSABE}/g" $TABELA
-sed -i -e "s/DIR-MASC-SIM/${SIM}/g" \
+	-e "s/PDIR-MASC-NSABE/${PNSABE}/g" \
+	-e "s/DIR-MASC-SIM/${SIM}/g" \
 	-e "s/DIR-MASC-NAO/${NAO}/g" \
 	-e "s/DIR-MASC-NSABE/${NSABE}/g" $TABELA
 
@@ -39,8 +39,8 @@ PNAO=$(echo "scale=${ESCALA}; ${NAO}*100 / 7" | bc -l)
 PNSABE=$(echo "scale=${ESCALA}; ${NSABE}*100 / 7" | bc -l)
 sed -i -e "s/PDIR-HOMO-SIM/${PSIM}/g" \
 	-e "s/PDIR-HOMO-NAO/${PNAO}/g" \
-	-e "s/PDIR-HOMO-NSABE/${PNSABE}/g" $TABELA
-sed -i -e "s/DIR-HOMO-SIM/${SIM}/g" \
+	-e "s/PDIR-HOMO-NSABE/${PNSABE}/g" \
+	-e "s/DIR-HOMO-SIM/${SIM}/g" \
 	-e "s/DIR-HOMO-NAO/${NAO}/g" \
 	-e "s/DIR-HOMO-NSABE/${NSABE}/g" $TABELA
 
@@ -53,8 +53,8 @@ PNAO=$(echo "scale=${ESCALA}; ${NAO}*100 / 46" | bc -l)
 PNSABE=$(echo "scale=${ESCALA}; ${NSABE}*100 / 46" | bc -l)
 sed -i -e "s/PBUR-EXT-SIM/${PSIM}/g" \
 	-e "s/PBUR-EXT-NAO/${PNAO}/g" \
-	-e "s/PBUR-EXT-NSABE/${PNSABE}/g" $TABELA
-sed -i -e "s/BUR-EXT-SIM/${SIM}/g" \
+	-e "s/PBUR-EXT-NSABE/${PNSABE}/g" \
+	-e "s/BUR-EXT-SIM/${SIM}/g" \
 	-e "s/BUR-EXT-NAO/${NAO}/g" \
 	-e "s/BUR-EXT-NSABE/${NSABE}/g" $TABELA
 
@@ -66,8 +66,8 @@ PNAO=$(echo "scale=${ESCALA}; ${NAO}*100 / 46" | bc -l)
 PNSABE=$(echo "scale=${ESCALA}; ${NSABE}*100 / 46" | bc -l)
 sed -i -e "s/PBUR-MASC-SIM/${PSIM}/g" \
 	-e "s/PBUR-MASC-NAO/${PNAO}/g" \
-	-e "s/PBUR-MASC-NSABE/${PNSABE}/g" $TABELA
-sed -i -e "s/BUR-MASC-SIM/${SIM}/g" \
+	-e "s/PBUR-MASC-NSABE/${PNSABE}/g" \
+	-e "s/BUR-MASC-SIM/${SIM}/g" \
 	-e "s/BUR-MASC-NAO/${NAO}/g" \
 	-e "s/BUR-MASC-NSABE/${NSABE}/g" $TABELA
 
@@ -79,8 +79,8 @@ PNAO=$(echo "scale=${ESCALA}; ${NAO}*100 / 46" | bc -l)
 PNSABE=$(echo "scale=${ESCALA}; ${NSABE}*100 / 46" | bc -l)
 sed -i -e "s/PBUR-HOMO-SIM/${PSIM}/g" \
 	-e "s/PBUR-HOMO-NAO/${PNAO}/g" \
-	-e "s/PBUR-HOMO-NSABE/${PNSABE}/g" $TABELA
-sed -i -e "s/BUR-HOMO-SIM/${SIM}/g" \
+	-e "s/PBUR-HOMO-NSABE/${PNSABE}/g" \
+	-e "s/BUR-HOMO-SIM/${SIM}/g" \
 	-e "s/BUR-HOMO-NAO/${NAO}/g" \
 	-e "s/BUR-HOMO-NSABE/${NSABE}/g" $TABELA
 
